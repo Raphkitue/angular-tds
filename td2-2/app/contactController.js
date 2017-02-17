cd07e0a516df55c08702abb3b4b7e342feff8172
Raphkitue <marionumber14@gmail.com>
2017-02-17 10:30:17 +0100

    Ajout application Contacts
    Ajout de l'application contact qui contient les visuels sans liste de
    contacts

---
+++ td2-2/app/contactController.js
@@ -0,0 +1,27 @@
+/**
+ * Created by raph_ on 17/02/2017.
+ */
+angular.module("ContactApp").controller("ContactController",function(){
+
+    this.contacts = [];
+    this.contact = '';
+    this.tmpContact = '';
+    this.operation ='';
+    this.edit ='';
+    this.toUpdate = function(){
+
+    }
+    this.toAdd = function () {
+
+    }
+    this.add = function () {
+    }
+
+    this.update= function () {
+
+    }
+
+    this.delete = function () {
+
+    }
+});
